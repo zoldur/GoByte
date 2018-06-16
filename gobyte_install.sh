949f5f7990288a90080a37d2f923bfcf1a04fd46
Zoldur <zoldur99@gmail.com>
2018-06-16 03:17:19 -0700

    Get IP

--- gobyte_install.sh
+++ gobyte_install.sh
@@ -12,7 +12,7 @@ COIN_NAME='Gobyte'
 COIN_PORT=12455
 RPC_PORT=12454
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -161,7 +161,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
